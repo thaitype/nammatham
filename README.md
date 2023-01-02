be4943244604fb5c1d56b7609c08cc2337500d84
Thada Wangthammang <thada.wth@gmail.com>
2023-01-02 10:01:07 +0700

    init example

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # az-functions
 Azure Function Nodejs frameworks with Dependency Injection
+
+## Dev
+
+```
+npx ts-node examples/crud/main.ts
+```
---
+++ examples/crud/main.ts
@@ -0,0 +1,9 @@
+import { Container } from 'inversify';
+
+// set up container
+const container = new Container();
+
+// set up bindings
+// container.bind<FooService>('FooService').to(FooService);
+
+console.log('sss')
