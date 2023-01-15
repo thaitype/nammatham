d0d21ec1aee9b1cbd8eb06d7a6d58683bad8e296
Thada Wangthammang <thada.wth@gmail.com>
2023-01-15 15:37:13 +0700

    feat: add routing config

--- src/bindings.ts
+++ src/bindings.ts
@@ -5,30 +5,35 @@ export type RequestMethod = "get" | "post" | "delete" | "options" | "put";
 export type BindingType = "httpTrigger" | "http";
 export type BindingDiection = "in" | "out";
 
+// Ref: https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook-trigger?tabs=in-process%2Cfunctionsv2&pivots=programming-language-javascript#configuration
 export interface FunctionBinding {
   authLevel?: AuthorizationLevel;
   type: BindingType;
   direction: BindingDiection;
   name: string;
   methods?: RequestMethod[];
+  route?: string;
 }
 
 export function httpTrigger(
   authLevel: AuthorizationLevel,
-  methods: RequestMethod[]
+  methods: RequestMethod[],
+  route?: string
 ): [FunctionBinding, FunctionBinding] {
-  return [
-    {
-      authLevel,
-      type: "httpTrigger",
-      direction: "in",
-      name: "req",
-      methods,
-    },
-    {
-      type: "http",
-      direction: "out",
-      name: "res",
-    },
-  ];
+  const requestBinding: FunctionBinding = {
+    authLevel,
+    type: "httpTrigger",
+    direction: "in",
+    name: "req",
+    methods,
+  };
+  if(route !== undefined){
+    requestBinding.route = route;
+  }
+  const responseBinding: FunctionBinding = {
+    type: "http",
+    direction: "out",
+    name: "res",
+  };
+  return [requestBinding, responseBinding];
 }
