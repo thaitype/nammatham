317080915cd30d412f43387d52a40615a6d225cc
Thada Wangthammang <thada.wth@gmail.com>
2023-02-06 08:49:39 +0700

    feat: check the func object is `execute` method

--- packages/core/src/bootstrap/function-bootstrap.ts
+++ packages/core/src/bootstrap/function-bootstrap.ts
@@ -26,6 +26,15 @@ export function funcBootstrap(option: IFuncBootstrapOption) {
   const controllerInstance = container.getNamed<BaseFunction<any>>(TYPE.Controller, option.classTarget.name);
   // Set context to in
   controllerInstance.init(azureFunctionContext);
+  // TODO: Move Check to check on build processs
+  if(!(controllerInstance.hasOwnProperty('execute'))){
+    console.error( `No execute method overrided in the BaseFunction`)
+    azureFunctionContext.res = {
+      status: 500,
+      body: `No execute method overrided in the BaseFunction`
+    }
+    return;
+  }
   // Fix method when execute the function
   controllerInstance.execute(...azureFunctionArgs);
 }
