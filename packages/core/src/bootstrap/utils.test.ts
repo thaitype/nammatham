5f705b10d5db79b68a612947c7adf192c6edfecf
Thada Wangthammang <thada.wth@gmail.com>
2023-02-11 13:26:54 +0700

    feat: add code cov

--- packages/core/src/bootstrap/utils.test.ts
+++ packages/core/src/bootstrap/utils.test.ts
@@ -1,9 +1,13 @@
 import test from 'ava';
-import { extractRelativeWorkingDirectory } from './utils';
+import { extractRelativeWorkingDirectory, removeExtension } from './utils';
 
 test('extractRelativeWorkingDirectory', t => {
   t.is(
     extractRelativeWorkingDirectory('/home/nammatham/examples/crud', '/home/nammatham/examples/crud/src/main.ts'),
     '/src'
   );
+});
+
+test('removeExtension', t => {
+  t.is(removeExtension('main.ts'), 'main');
 });
