a083c1080f3a18eefc8bd0faa288e2a6915645cb
Thada Wangthammang <thada.wth@gmail.com>
2023-02-18 18:32:38 +0700

    feat: add cosmosDBTrigger both v2 and v4 helper functions

--- packages/core/src/bindings/binding/index.ts
+++ packages/core/src/bindings/binding/index.ts
@@ -2,6 +2,7 @@ import { custom } from './custom';
 import { httpTrigger } from './http-trigger';
 import { http, httpWithReturn } from './http';
 import { timerTrigger } from './timer-trigger';
+import { cosmosDBTrigger, cosmosDBTrigger_v2, cosmosDBTrigger_v4 } from './cosmos-db-trigger';
 
 export default {
   httpTrigger,
@@ -9,4 +10,7 @@ export default {
   httpWithReturn,
   timerTrigger,
   custom,
+  cosmosDBTrigger,
+  cosmosDBTrigger_v2,
+  cosmosDBTrigger_v4,
 };
--- packages/core/src/bindings/interfaces/function-binding.ts
+++ packages/core/src/bindings/interfaces/function-binding.ts
@@ -1,10 +1,12 @@
 import { CustomFunctionBinding } from './custom-function-binding';
-import { HttpTriggerBinding, HttpBinding, TimerTriggerBinding } from './triggers';
+import { HttpTriggerBinding, HttpBinding, TimerTriggerBinding, CosmosDBTriggerBinding_v2, CosmosDBTriggerBinding_v4 } from './triggers';
 
 export type DefinedFunctionBinding<T extends unknown> =
   | HttpTriggerBinding<T>
   | HttpBinding<T>
-  | TimerTriggerBinding<T>;
+  | TimerTriggerBinding<T>
+  | CosmosDBTriggerBinding_v2<T>
+  | CosmosDBTriggerBinding_v4<T>;
 
 /**
  * If `T` type is `null`, then return `DefinedFunctionBinding`,
