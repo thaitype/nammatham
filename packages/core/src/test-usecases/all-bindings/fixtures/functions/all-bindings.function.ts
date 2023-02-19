feb627fa4b2d4f7d36e81bd323e8c2e966b5def5
Thada Wangthammang <thada.wth@gmail.com>
2023-02-19 09:13:07 +0700

    test: add more test case for cosmos db trigger

--- packages/core/src/test-usecases/all-bindings/fixtures/functions/all-bindings.function.ts
+++ packages/core/src/test-usecases/all-bindings/fixtures/functions/all-bindings.function.ts
@@ -1,4 +1,4 @@
-import { BaseFunction, Binding, functionName } from '../../../../main';
+import { BaseFunction, Binding, CosmosDBTriggerInputBinding_v2, functionName } from '../../../../main';
 import { responseHelper } from '../../../response-helper';
 import { HttpRequest, HttpResponse, Timer} from '@azure/functions';
 
@@ -8,10 +8,41 @@ const bindings = [
   Binding.httpWithReturn(),
   Binding.timerTrigger({ name: 'timer' as const, schedule: '*' }), // make string to literal type
   Binding.cosmosDBTrigger_Input_v2({
+    name: 'document_input_v2' as const,
     collectionName: '',
     databaseName: '',
-    name: 'document_input_v2' as const,
     connectionStringSetting: '' ,
+  }),
+  Binding.cosmosDBTrigger_Output_v2({
+    name: 'document_output_v2' as const,
+    collectionName: '',
+    createIfNotExists: true,
+    databaseName: '',
+    partitionKey: ''
+  }),
+  Binding.cosmosDBTrigger_Input_v4({
+    name: 'document_input_v4' as const,
+    connection: '',
+    containerName: '',
+    databaseName: '',
+  }),
+  Binding.cosmosDBTrigger_Output_v4({
+    name: 'document_output_v4' as const,
+    connection: '',
+    containerName: '',
+    databaseName: '',
+  }),
+  Binding.cosmosDBTrigger_Input({
+    name: 'document_input_default' as const,
+    connection: '',
+    containerName: '',
+    databaseName: '',
+  }),
+  Binding.cosmosDBTrigger_Output({
+    name: 'document_output_default' as const,
+    connection: '',
+    containerName: '',
+    databaseName: '',
   }),
 ] as const;
 
@@ -24,6 +55,11 @@ export class AllBindingsFunction extends BaseFunction<typeof bindings> {
     const res: HttpResponse = this.bindings.res;
     const timer: Timer = this.bindings.timer;
     const document_input_v2: any = this.bindings.document_input_v2;
+    const document_output_v2: any = this.bindings.document_output_v2;
+    const document_input_v4: any = this.bindings.document_input_v4;
+    const document_output_v4: any = this.bindings.document_output_v4;
+    const document_input_default: any = this.bindings.document_input_default;
+    const document_output_default: any = this.bindings.document_output_default;
     const { name } = this.req.query;
     this.res.send(responseHelper(name));
   }
