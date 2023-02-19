9417eba3e6cee972e7c1bdc2b596ffc648083ef3
Thada Wangthammang <thada.wth@gmail.com>
2023-02-19 09:19:46 +0700

    test: add more test service type

--- packages/core/src/test-usecases/http-trigger-with-services/fixtures/functions/with-type-utility.function.ts
+++ packages/core/src/test-usecases/http-trigger-with-services/fixtures/functions/with-type-utility.function.ts
@@ -2,6 +2,8 @@ import { inject } from 'inversify';
 import { BaseFunction, Binding, functionName } from '../../../../main';
 import { SingletonService } from '../services/singleton-service';
 import { responseHelper, serviceData } from '../../../response-helper';
+import { TransientService } from '../services/transient-service';
+import { ScopedService } from '../services/scoped-service';
 
 const bindings = [
   Binding.httpTrigger({ name: 'req' as const }), // make string to literal type
@@ -10,12 +12,23 @@ const bindings = [
 
 @functionName('WithTypeUtility', ...bindings)
 export class WithTypeUtilityFunction extends BaseFunction<typeof bindings> {
-  constructor(@inject(SingletonService) private service: SingletonService) {
+  constructor(
+    @inject(SingletonService) private singletonService: SingletonService,
+    @inject(TransientService) private transientService: TransientService,
+    @inject(ScopedService) private scopedService: ScopedService
+  ) {
     super();
   }
 
   public override execute() {
     const { name } = this.req.query;
-    this.res.send(responseHelper(name, this.service.getData(serviceData)));
+    this.res.send(
+      responseHelper(
+        name,
+        this.singletonService.getData(serviceData),
+        this.scopedService.getData(serviceData),
+        this.transientService.getData(serviceData)
+      )
+    );
   }
 }
