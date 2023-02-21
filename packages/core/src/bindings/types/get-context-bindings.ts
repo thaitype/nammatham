c3da357c14226fe8da647ad7a67b7fa840f651ac
Thada Wangthammang <thada.wth@gmail.com>
2023-02-21 22:03:20 +0700

    feat: infer input, output return binding type

--- packages/core/src/bindings/types/get-context-bindings.ts
+++ packages/core/src/bindings/types/get-context-bindings.ts
@@ -2,7 +2,7 @@ import { ContextBindings } from '@azure/functions';
 import { FunctionBinding } from '../interfaces/function-binding';
 import { BindingType } from './binding';
 import { Zipper } from '../../types';
-import { Binding, DirectionType } from '../../main';
+import { DirectionType } from '../../main';
 
 type GetTypeTuple<T extends readonly FunctionBinding<unknown>[]> = T extends readonly [infer Head, ...infer Tail]
   ? Head extends FunctionBinding<unknown>
@@ -20,85 +20,10 @@ type GetNameTuple<T extends readonly FunctionBinding<unknown>[]> = T extends rea
     : []
   : [];
 
-const bindings = [
-  Binding.httpTrigger({ name: 'req' as const }), // make string to literal type
-  Binding.http({ name: 'res' as const }), // make string to literal type
-  Binding.http_withReturn(),
-  Binding.timerTrigger({ name: 'timer' as const, schedule: '*' }), // make string to literal type
-  Binding.cosmosDBTrigger_v2({
-    name: 'document_trigger_v2' as const,
-    collectionName: '',
-    connection: '',
-    connectionStringSetting: '',
-    containerName: '',
-    databaseName: '',
-  }),
-  Binding.cosmosDBTrigger_v4({
-    name: 'document_trigger_v4' as const,
-    connection: '',
-    containerName: '',
-    databaseName: '',
-  }),
-  Binding.cosmosDBTrigger({
-    name: 'document_trigger_default' as const,
-    connection: '',
-    containerName: '',
-    databaseName: '',
-  }),
-  Binding.cosmosDB_output_v2({
-    name: 'document_output_v2' as const,
-    collectionName: '',
-    connectionStringSetting: '',
-    createIfNotExists: true,
-    databaseName: '',
-  }),
-  Binding.cosmosDB_output_v4({
-    name: 'document_output_v4' as const,
-    createIfNotExists: true,
-    databaseName: '',
-    connection: '',
-    containerName: '',
-  }),
-  Binding.cosmosDB_output({
-    name: 'document_output_default' as const,
-    createIfNotExists: true,
-    databaseName: '',
-    connection: '',
-    containerName: '',
-  }),
-  Binding.cosmosDB_input_v2({
-    name: 'document_input_v2' as const,
-    collectionName: '',
-    connectionStringSetting: '',
-    databaseName: '',
-    id: '',
-    partitionKey: '',
-    sqlQuery: '',
-  }),
-  Binding.cosmosDB_input_v4({
-    name: 'document_input_v4' as const,
-    databaseName: '',
-    id: '',
-    partitionKey: '',
-    sqlQuery: '',
-    connection: '',
-    containerName: '',
-  }),
-  Binding.cosmosDB_input({
-    name: 'document_input_default' as const,
-    databaseName: '',
-    id: '',
-    partitionKey: '',
-    sqlQuery: '',
-    connection: '',
-    containerName: '',
-  }),
-] as const;
-
-type FilterBindingsDirection<T extends readonly FunctionBinding<unknown>[], Direction extends DirectionType> = T extends readonly [
-  infer Head,
-  ...infer Tail
-]
+type FilterBindingsDirection<
+  T extends readonly FunctionBinding<unknown>[],
+  Direction extends DirectionType
+> = T extends readonly [infer Head, ...infer Tail]
   ? Head extends FunctionBinding<unknown>
     ? Tail extends FunctionBinding<unknown>[]
       ? Head['direction'] extends Direction
@@ -108,13 +33,56 @@ type FilterBindingsDirection<T extends readonly FunctionBinding<unknown>[], Dire
     : []
   : [];
 
-type A = FilterBindingsDirection<typeof bindings, 'out'>;
-  // ^?
 /**
  * `GetBindings`, extract type from `FunctionBinding<unknown>[]`
  */
 export type GetBindings<T extends readonly FunctionBinding<unknown>[]> = ContextBindings &
   Zipper<GetNameTuple<T>, GetTypeTuple<T>>;
 
-type B = GetBindings<FilterBindingsDirection<typeof bindings, 'out'>>;
-  // ^?
+export type GetOnlyOutputBindings<T extends readonly FunctionBinding<unknown>[]> = GetBindings<
+  FilterBindingsDirection<T, 'out'>
+>;
+export type GetOnlyInputBindings<T extends readonly FunctionBinding<unknown>[]> = GetBindings<
+  FilterBindingsDirection<T, 'in'>
+>;
+export type GetOnlyInputOutputBindings<T extends readonly FunctionBinding<unknown>[]> = GetBindings<
+  FilterBindingsDirection<T, 'inout'>
+>;
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+type GetBindingOnlyReturnName<T extends readonly FunctionBinding<unknown>[]> = T extends readonly [
+  infer Head,
+  ...infer Tail
+]
+  ? Head extends FunctionBinding<unknown>
+    ? Tail extends FunctionBinding<unknown>[]
+      ? Head['name'] extends '$return'
+        ? [Head]
+        : [...GetBindingOnlyReturnName<Tail>]
+      : []
+    : []
+  : [];
+
+type GetFirstArray<T extends any[]> = T['length'] extends 1 ? T[0] : undefined;
+
+/**
+ * Get Correct Type from Return Functions,
+ *
+ * If set the `name` property in function.json to `$return`, it will return only single output.
+ * Otherwise, it will return multiple output.
+ *
+ * Set the `name` property in function.json to `$return`. If there are multiple output bindings, use the return value for only one of them.
+ * Ref: https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-return-value?tabs=javascript
+ */
+
+export type GetOnlyReturnBindings<T extends readonly FunctionBinding<unknown>[]> = GetFirstArray<
+  GetBindingOnlyReturnName<T>
+> extends infer Item
+  ? Item extends undefined
+    ? GetOnlyOutputBindings<T>
+    : Item extends FunctionBinding<unknown>
+    ? BindingType<Item['type']>
+    : never
+  : never;
