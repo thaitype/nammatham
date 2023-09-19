ee853ca9942a6322de447ca96a9c0e6cb2f34633
Thada Wangthammang <thada.wth@gmail.com>
2023-09-19 19:46:26 +0700

    poc api

--- examples/with-functional/src/functions/copyBlob1.ts
+++ examples/with-functional/src/functions/copyBlob1.ts
@@ -1,6 +1,5 @@
 // import { app, input, InvocationContext, output, StorageQueueTriggerOptions } from '@azure/functions';
 
-
 // const blobInput = input.storageBlob({
 //   connection: 'storage_APPSETTING',
 //   path: 'helloworld/{queueTrigger}',
@@ -23,30 +22,55 @@
 //     context.extraOutputs.set(blobOutput, blobInputValue);
 //   },
 // });
+// class NammathamFactory {
+//   // static create(adapter: Adapter) {
+//   //   return new NammathamApp(adapter);
+//   // }
+//   static createFunc() {
+//     return new NammathamFunction();
+//   }
+// }
 
-import { InvocationContext, StorageQueueTriggerOptions } from "@azure/functions";
 
+import { InvocationContext, StorageQueueTriggerOptions } from '@azure/functions';
 
-class NammathamApp {
+const initNammatham = {
+  create() {
+    return new NammathamFunction();
+  }
+}
+
+class NammathamContext   {
+  constructor(public invocationContext: InvocationContext) {}
+
+  /**
+     * The recommended way to log information data (level 2) during invocation.
+     * Similar to Node.js's `console.info`, but has integration with Azure features like application insights
+     */
+  public log(...args: any[]) {
+    this.invocationContext.log(...args);
+  }
+}
+
+export type TriggerType =
+  | 'http'
+  | 'timer'
+  | 'storageBlob'
+  | 'storageQueue'
+  | 'serviceBusQueue'
+  | 'serviceBusTopic'
+  | 'eventHub'
+  | 'eventGrid'
+  | 'cosmosDB';
+
+class NammathamFunction {
   constructor(public name?: string) {}
 
-  trigger(
-    type:
-      | 'http'
-      | 'timer'
-      | 'storageBlob'
-      | 'storageQueue'
-      | 'serviceBusQueue'
-      | 'serviceBusTopic'
-      | 'eventHub'
-      | 'eventGrid'
-      | 'cosmosDB',
-    option: any
-  ) {
+  trigger(type: TriggerType, option: any) {
     return this;
   }
 
-  storageQueue(name: string, option: StorageQueueTriggerOptions) {
+  storageQueue(funcName: string, option: StorageQueueTriggerOptions) {
     return this;
   }
 
@@ -58,37 +82,34 @@ class NammathamApp {
     return this;
   }
 
-  handler(func: (queueItem: unknown, context: InvocationContext) => void) {
+  addExtraInputs(input: any){
+    return this;
+  }
+
+  handler(func: (queueItem: unknown, context: NammathamContext) => void) {
     throw new Error('Function not implemented.');
   }
 }
 
-const app4 = new NammathamApp('CopyBlob')
-  .trigger('storageQueue', {
+const app4 = initNammatham.create()
+  .storageQueue('CopyBlob', {
     queueName: 'copyblobqueue',
     connection: 'storage_APPSETTING',
   })
-  .addExtraInput('storageBlob', {
-    connection: 'storage_APPSETTING',
-    path: 'helloworld/{queueTrigger}',
+  .addExtraInput('blobInput', {
+      type: 'storageBlob',
+      connection: 'storage_APPSETTING',
+      path: 'helloworld/{queueTrigger}-copy',
   })
   .addExtraOutput('storageBlob', {
     connection: 'storage_APPSETTING',
     path: 'helloworld/{queueTrigger}-copy',
   })
-  .handler((queueItem: unknown, context: InvocationContext) => {
+  .handler((queueItem: unknown, context: NammathamContext) => {
     context.log('Storage queue function processed work item:', queueItem);
+    const blobInputValue = context.extraInputs.get('blobInput');
 
-    const blobInputValue = context.extraInputs.get('storageBlob'); 
-    
     context.extraOutputs.set('storageBlob', blobInputValue);
-  },);
+  });
 
-// const app3 = new NammathamApp()
-//   .storageQueue('storageQueue', {
-//     queueName: 'copyblobqueue',
-//     connection: 'storage_APPSETTING',
-//   })
-//   .addExtraInput()
-//   .addExtraOutput()
-//   .handler();
+  const app5 = initNammatham.create()
