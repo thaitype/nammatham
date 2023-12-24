d17a99d200d9a77e8487522ffac5d50058f7a994
Thada Wangthammang <thada.wth@gmail.com>
2023-12-24 12:22:03 +0700

    feat: add signature for add function for nammatham app

--- examples/with-functional/src/main.ts
+++ examples/with-functional/src/main.ts
@@ -1,4 +1,8 @@
+import blob from './functions/blob';
+import hello from './functions/hello';
 import { app } from './nammatham';
 
-app.addFunctionsFromPath('functions/*.ts');
+// app.addFunctionsFromPath('functions/*.ts'); // TODO: Implement in next release
+app.addFunctions(blob);
+app.addFunction(hello);
 app.start();
--- packages/core/src/core/nammatham-app.ts
+++ packages/core/src/core/nammatham-app.ts
@@ -1,9 +1,17 @@
+import type { NammathamFunctionEndpoint } from "./types";
+
 export class NammathamApp {
   start() {
     console.log('Starting app');
   }
-  addFunctionsFromPath(path: string) {
-    console.log('addFunctionsFromPath', path);
-    return this;
+  
+  addFunctions(...functions: NammathamFunctionEndpoint<any, any>[]) {
+    for(const func of functions) {
+      this.addFunction(func);
+    }
+  }
+
+  addFunction<TTriggerType, TReturnType>(func: NammathamFunctionEndpoint<TTriggerType, TReturnType>) {
+    console.log('Adding function', func);
   }
 }
