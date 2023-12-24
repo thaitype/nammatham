2952273705fe11a063b3678bd647802406504d74
Thada Wangthammang <thada.wth@gmail.com>
2023-12-24 09:33:38 +0700

    refactor: move handler inside handler

--- examples/basic/src/functions/hello.ts
+++ examples/basic/src/functions/hello.ts
@@ -13,5 +13,11 @@ export default createFunction('hello', {
   type: 'http',
   methods: ['GET', 'POST'],
   authLevel: 'anonymous',
-  handler: hello,
-});
+  handler: async (request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> => {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || (await request.text()) || 'world';
+
+    return { body: `Hello, ${name}!` };
+  },
+})
