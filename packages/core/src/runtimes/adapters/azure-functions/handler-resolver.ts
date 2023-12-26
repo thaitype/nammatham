6f282acb7350f5c0bfc2a59c310a276a4e25b541
Thada Wangthammang <thada.wth@gmail.com>
2023-12-26 20:56:36 +0700

    feat: add log when function is executed (dev server)

--- packages/core/src/runtimes/adapters/azure-functions/handler-resolver.ts
+++ packages/core/src/runtimes/adapters/azure-functions/handler-resolver.ts
@@ -5,6 +5,7 @@ import { PromiseLike } from '../../types';
 import type { Request, Response } from 'express';
 import { HttpRequest } from './http/HttpRequest';
 import { NammathamApp } from '../../nammatham-app';
+import { v4 as uuidv4 } from 'uuid';
 
 export class AzureFunctionsHandlerResolver extends BaseHandlerResolver {
   override async resolveHandler(
@@ -12,10 +13,15 @@ export class AzureFunctionsHandlerResolver extends BaseHandlerResolver {
     req: Request,
     res: Response
   ) {
-    console.log(`Starting using Azure Functions handler resolver`);
-    // console.log(`Running handler from `, endpoint);
-    // TODO: Mock Azure Functions's Http Request and InvocationContext
-    return await endpoint.invokeHandler(new HttpRequest(req), this.mockInvocationContext());
+    const context = new InvocationContext({
+      invocationId: uuidv4(),
+      functionName: endpoint.name
+    });
+    console.log(`Executing 'Functions.${endpoint.name}' (Reason='This function was programmatically called via the host APIs.', Id=${context.invocationId})`);
+    return endpoint.invokeHandler(new HttpRequest(req), context).then((result: any) => {
+      console.log(`Executed 'Functions.${endpoint.name}' (Succeeded, Id=${context.invocationId}, Duration=${0}ms)`);
+      return result;
+    });
   }
 
   override async resolveRegisterHandler(app: NammathamApp) {
