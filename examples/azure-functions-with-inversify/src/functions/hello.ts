faf1832905b23e83a65359a6997a937b6913f13e
Thada Wangthammang <thada.wth@gmail.com>
2024-01-02 23:59:10 +0700

    example: implement example for inversify wrapper

--- examples/azure-functions-with-inversify/src/functions/hello.ts
+++ examples/azure-functions-with-inversify/src/functions/hello.ts
@@ -1,133 +1,31 @@
-import { interfaces } from 'inversify';
-import { Tokens } from '../constants';
+import { Tokens, Option } from '../constants';
 import { func } from '../nammatham';
 import { DataService } from '../services/data';
 import { Service } from '../services/service';
-
-type ResolveItem<T = any> = {
-  serviceIdentifier: interfaces.ServiceIdentifier<T>;
-  type: any;
-};
-
-interface InversifyProvider<T extends Record<string, unknown>> {
-  resolve: T;
-}
-
-type InversifyProviderFunc<T extends Record<string, unknown>, TReturn> = (services: T) => TReturn;
-
-declare function inversify<T extends Record<string, unknown>, TReturn>(
-  arg: InversifyProvider<T>,
-  func: InversifyProviderFunc<T, TReturn>
-): TReturn;
+import { inversify } from './inversify-2';
+import { container } from '../container';
 
 export default func
   .httpGet('hello', {
     route: 'hello-world',
   })
   .handler(
-    inversify(
-      {
-        resolve: {
-          dataService: DataService.prototype,
-        },
-      },
-      ({ dataService }) =>
-        async (request, ctx) => {
-          ctx.context.log('HTTP trigger function processed a request.');
-          ctx.context.debug(`Http function processed request for url "${request.url}"`);
-          const name = request.query.get('name') || (await request.text()) || 'world';
-          if (name === 'error') {
-            throw new Error('this is an error');
-          }
-          return {
-            body: dataService.getData(),
-          };
+    inversify(container)
+      .inject<DataService>().with('dataService', DataService)
+      .inject<Service>().with('service', Service)
+      .inject<Option>().with('option', Tokens.Option)
+      .resolve(({ dataService, option, service }) => async (request, ctx) => {
+        ctx.context.log('HTTP trigger function processed a request.');
+        ctx.context.debug(`Http function processed request for url "${request.url}"`);
+        const name = request.query.get('name') || (await request.text()) || 'world';
+        if (name === 'error') {
+          throw new Error('this is an error');
         }
-    )
+        console.log(`service.getData()`, service.getData());
+        console.log(`dataService.getData()`, dataService.getData());
+        console.log(`option`, option);
+        return {
+          body: dataService.getData(),
+        };
+      })
   );
-
-// Draft - type safe version
-
-type InversifyProviderFuncMock<T extends Record<string, unknown>, TReturn> = (services: T) => TReturn;
-
-declare function inversify2(...args: any[]): {
-  addProviders: <T extends Record<string, unknown>>(
-    services: T
-  ) => {
-    resolve: <TReturn>(func: InversifyProviderFuncMock<T, TReturn>) => TReturn;
-  };
-};
-
-typeof DataService.prototype === typeof DataService; // False T_T
-
-type ConvertClassFunctionToObject<T> = {
-  [P in keyof T]: T[P];
-};
-
-type ConvertClassFunctionToObject2<T> = 'prototype' extends keyof T ? T['prototype'] : T;
-
-type MyDataService = ConvertClassFunctionToObject<DataService>;
-type MyDataService2 = ConvertClassFunctionToObject2<DataService>;
-function test(service: MyDataService2){
-  service.getData();
-}
-
-inversify2()
-  .addProviders({
-    dataService: { serviceIdentifier: DataService, type: DataService },
-    service: { serviceIdentifier: Tokens.Service, type: Service },
-  })
-  .resolve(({ dataService, service }) => async (request, ctx) => {
-    return {
-      body: dataService.getData(),
-    };
-  });
-
-// another version (I think best version)
-type MockResolveType = {
-  dataService: DataService;
-  service: string;
-};
-// @ts-ignore
-inversify3(container)
-  .inject<DataService>({ dataService: DataService })
-  .inject<string>({ service: Tokens.Service })
-  .resolve(({ dataService, service }: MockResolveType) => async (request, ctx) => {
-    return {
-      body: dataService.getData(),
-    };
-  });
-
-// Draft - non type safe version
-
-declare function inversify3(...args: any[]): any;
-
-// v3.1
-inversify3()
-  .addProviders([DataService, Tokens.Service])
-  .resolve((dataService: DataService, service: string) => async (request, ctx) => {
-    return {
-      body: dataService.getData(),
-    };
-  });
-
-// v3.2
-inversify3({
-  providers: [DataService, Tokens.Service],
-  resolve: (dataService: DataService, service: string) => async (request, ctx) => {
-    return {
-      body: dataService.getData(),
-    };
-  },
-});
-
-inversify3({
-  providers: [DataService, Tokens.Service],
-  resolve(dataService: DataService, service: string) {
-    return async (request, ctx) => {
-      return {
-        body: dataService.getData(),
-      };
-    };
-  },
-});
--- examples/azure-functions-with-inversify/src/functions/inversify-2.ts
+++ examples/azure-functions-with-inversify/src/functions/inversify-2.ts
@@ -4,6 +4,7 @@ import { DataService } from '../services/data';
 import { Service } from '../services/service';
 import { Tokens, Option } from '../constants';
 import { container } from '../container';
+type InversifyProviderFunc<T extends Record<string, unknown>, TReturn> = (services: T) => TReturn;
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 class InversifyProvider<Items extends Record<string, interfaces.ServiceIdentifier<unknown>> = {}> {
@@ -15,8 +16,12 @@ class InversifyProvider<Items extends Record<string, interfaces.ServiceIdentifie
     return new Injection<OutputType, Items>(this);
   }
 
-  resolve<TReturn>(func: (...args: any[]) => any): TReturn {
-    return func();
+  resolve<TReturn>(func: InversifyProviderFunc<Items, TReturn>): TReturn {
+    const services: Record<string, unknown> = {};
+    Object.keys(this.items).forEach(key => {
+      services[key] = this.container.get(this.items[key]);
+    });
+    return func(services as Items & Record<string, unknown>);
   }
 }
 
@@ -31,26 +36,20 @@ class Injection<OutputType, Items extends Record<string, interfaces.ServiceIdent
       ...this.provider.items,
       [name]: item,
     };
-    return this.provider as InversifyProvider<
-      Items & Record<Name, interfaces.ServiceIdentifier<WithOutputType>>
-    >;
+    return this.provider as InversifyProvider<Items & Record<Name, WithOutputType>>;
   }
 }
 
-function inversify(container: Container) {
+export function inversify(container: Container) {
   return new InversifyProvider(container);
 }
 
 const result = inversify(container)
   .inject<DataService>().with('dataService', DataService)
   .inject<Service>().with('service', Service)
-  .inject<Option>().with('option', Tokens.Option);
-
-// const service = container.get(result.items.service);
-// console.log(`service.getData()`, service.getData());
-
-// const dataService = container.get(result.items.dataService);
-// console.log(`dataService.getData()`, dataService.getData());
-
-// const option = container.get(result.items.option);
-// console.log(`option`, option);
+  .inject<Option>().with('option', Tokens.Option)
+  .resolve(({ dataService, service, option }) => {
+    console.log(`service.getData()`, service.getData());
+    console.log(`dataService.getData()`, dataService.getData());
+    console.log(`option`, option);
+  });
