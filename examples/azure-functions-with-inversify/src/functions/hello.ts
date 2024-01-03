afc1f130e91d3cfd8781665712511bd20699fc65
Thada Wangthammang <thada.wth@gmail.com>
2024-01-03 14:17:57 +0700

    feat: add package `@di-extra/inversify`

--- examples/azure-functions-with-inversify/src/functions/hello.ts
+++ examples/azure-functions-with-inversify/src/functions/hello.ts
@@ -2,10 +2,10 @@ import { Tokens, Option } from '../constants';
 import { func } from '../nammatham';
 import { DataService } from '../services/data';
 import { Service } from '../services/service';
-import { inversify } from './inversify-3';
+import { injector } from '@di-extra/inversify';
 import { container } from '../container';
 
-const providers = inversify(container)
+const providers = injector(container)
   .inject('dataService', DataService).to<DataService>()
   .inject('service', Service).to<Service>()
   .inject('option', Tokens.Option).to<Option>()
@@ -30,3 +30,7 @@ export default func
       };
     })
   );
+
+
+
+  
