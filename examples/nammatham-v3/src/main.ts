c5a1e2b160b9de18630406c802bf9e4164ddf17e
Thada Wangthammang <thada.wth@gmail.com>
2024-02-20 23:17:16 +0700

    feat: add handle function for v3

--- examples/nammatham-v3/src/main.ts
+++ examples/nammatham-v3/src/main.ts
@@ -1,3 +1,26 @@
-import { test } from 'nammatham-v3';
+// PoC version for combining between v2 and v3 prposal
+import { nammatham } from 'nammatham-v3';
+import { Hono } from 'hono';
 
-console.log(test);
+const func = nammatham.create();
+const app = new Hono();
+
+app.get('/', c => {
+  return c.text('Hello, World!');
+});
+
+const helloFunction = func.timer('myTimer', { schedule: '0 */5 * * * *' }).handler(async c => {
+  if (c.trigger.isPastDue) {
+    console.log('Past due');
+  }
+});
+
+const dev = process.env.NODE_ENV === 'development';
+
+nammatham.handle({
+  dev,
+  app,
+  triggers: {
+    helloFunction,
+  },
+});
--- packages/v3/src/main.ts
+++ packages/v3/src/main.ts
@@ -1,4 +1,29 @@
-export type Info = {
-  info: string;
+import type { Hono } from 'hono';
+
+import { type BaseHandler } from '@nammatham/core';
+import { AzureFunctionsAdapter } from '@nammatham/azure-functions';
+
+interface NammathamHandleOptions {
+  dev?: boolean;
+  app?: Hono;
+  triggers?: Record<string, BaseHandler<any>>;
+}
+
+const adapter = new AzureFunctionsAdapter();
+
+export const nammatham = {
+  create() {
+    return adapter.createTrigger();
+  },
+
+  handle(option: NammathamHandleOptions) {
+    const nammathamApp = adapter.createApp();
+    nammathamApp.setDevelopment(option.dev ?? false);
+    if (option.triggers) {
+      nammathamApp.addFunctions(...Object.values(option.triggers));
+    }
+    // TODO: Use Hono as a dev server
+    // nammathamApp.register(expressplugin());
+    nammathamApp.start();
+  },
 };
-export const test = 'test';
