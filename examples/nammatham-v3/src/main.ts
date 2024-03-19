5ca379a0ae568a875f298698aa6a4641f7310120
Thada Wangthammang <thada.wth@gmail.com>
2024-03-19 21:15:43 +0700

    poc v3 example

--- examples/nammatham-v3/src/main.ts
+++ examples/nammatham-v3/src/main.ts
@@ -1,7 +1,7 @@
 // PoC version for combining between v2 and v3 proposal
 import { Nammatham } from 'nammatham';
 import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
+import { Context, Hono, HonoRequest } from 'hono';
 
 const dev = process.env.NODE_ENV === 'development';
 const port = 3000;
@@ -13,6 +13,15 @@ const app = new Hono();
 app.get('/', c => {
   return c.text('Hello, World!');
 });
+
+app.use(async (c, next) => {
+  const start = Date.now()
+  await next()
+  const end = Date.now()
+
+  console.log(`Time: ${end - start}ms`)
+  // c.res.headers.set('X-Response-Time', `${end - start}`)
+})
 
 nammatham.addFunction(
   func.timer('myTimer', { schedule: '0 */5 * * * *' }).handler(async c => {
@@ -27,3 +36,10 @@ nammatham.useNodeServer(serve);
 
 // For Node.js
 nammatham.handle(app);
+
+app.routes.forEach(route => {
+  console.log(`Route: ${route.path}`);
+  route.handler({
+    text: (text: string) => console.log(text),
+  } as any, async () => console.log('done'));
+});
